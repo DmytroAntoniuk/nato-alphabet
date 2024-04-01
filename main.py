28c7054b051f4b585c5a0d365b11fea2c102ec1c
DmytroAntoniuk <8antoniuk8@gmail.com>
2024-04-01 22:26:55 +0300

    exceptions handling added

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ nato_phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in nato_phonetic_alphabet.iterrows()}
 
-user_input = input("Enter a word:").upper()
-output_list = [phonetic_dict[letter] for letter in user_input]
+def generate_phonetic():
+    try:
+        user_input = input("Enter a word:").upper()
+        output_list = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-print(output_list)
+generate_phonetic()
